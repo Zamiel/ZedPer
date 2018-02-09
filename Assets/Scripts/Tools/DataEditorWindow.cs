481bae7490e307b63fa93cc8fc1e01253d55fd2a
Spardason <eric-vaillancourt@hotmail.ca>
2018-02-09 16:13:55 -0500

    First Commit

---
+++ Assets/Scripts/Tools/DataEditorWindow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class DataEditorWindow : EditorWindow
+{
+    [MenuItem("ZedPer/Data Editor")]
+    static void Init()
+    {
+        EditorWindow.GetWindow(typeof(DataEditorWindow));
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void OnGUI()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Utils/Attributes/DisplayInDataEditor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class DisplayInDataEditor : Attribute
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
